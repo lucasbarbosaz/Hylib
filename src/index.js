0f0dd764d0b8422ee5c0c5e219d86b67d53d8551
Lucas Barbosa <lucashp03@gmail.com>
2023-03-23 18:11:41 -0300

    logging user out if token signature is invalid

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+import AuthService from './services/AuthService';
 
 var xhttp = new XMLHttpRequest();
 var configuration = {};
@@ -14,6 +15,20 @@ xhttp.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     configuration = JSON.parse(xhttp.responseText);
     axios.defaults.baseURL = configuration.apiUrl;
+
+    axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        // Check the response for an invalid JWT token error
+        if (error.response.status === 401 && error.response.data.error === 'Invalid token') {
+          // Remove JWT token from local storage
+          AuthService.logout();
+          
+          // Redirect the user to the login page
+          window.location.href = '/logout';
+        }
+      }
+    )
     ReactDOM.render(
       <React.StrictMode>
         <App config={JSON.stringify(configuration)} />
